443dccdf3792c90c646b8220e66ab7bd42cd226b
PigeonAssassin <119262062+PigeonAssassin@users.noreply.github.com>
2023-10-24 21:39:07 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "832379703292731392"; // your league ID
+export const leagueID = "917157569830088704"; // your league ID
 export const leagueName = "No Punt Intended!"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
